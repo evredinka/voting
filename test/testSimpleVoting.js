571b66955dfa2065560523822f522415483efeda
Evgeniia Prozorova <evgeniia.prozorova@gmail.com>
2019-06-27 16:59:45 +0200

    more unit tests

--- test/testSimpleVoting.js
+++ test/testSimpleVoting.js
@@ -2,7 +2,7 @@ const SimpleVoting = artifacts.require("./SimpleVoting.sol");
 
 contract('SimpleVoting', accounts => {
     contract('SimpleVoting.admin', accounts => {
-        it('must be the user who deployed the contract', async function() {
+        it('must be the user (via variable) who deployed the contract', async () => {
             let instance = await SimpleVoting.deployed();
             let admin = await instance.admin();
 
@@ -10,5 +10,22 @@ contract('SimpleVoting', accounts => {
 
             assert.isTrue(admin == votingAdmin);
         })
+
+        it('must be the user (via getter) who deployed the contract', async () => {
+            let instance = await SimpleVoting.deployed();
+
+            let isAdmin = await instance.isAdministrator(web3.eth.accounts[0])
+
+            assert.isTrue(isAdmin);
+        })
+
+        it("must NOT be the user (via getter) who didn't deploy the contract", async () => {
+            let instance = await SimpleVoting.deployed();
+
+            let isAdmin = await instance.isAdministrator(web3.eth.accounts[1])
+
+            assert.isFalse(isAdmin);
+        })
     })
+
 })
